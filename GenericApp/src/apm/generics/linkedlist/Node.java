317fb047b7d86094a1b8ccf26f5991aa46143659
erashishmodi <i_m_ashish@yahoo.com>
2014-10-27 02:05:53 +0530

    Uppdated

---
+++ GenericApp/src/apm/generics/linkedlist/Node.java
@@ -0,0 +1,19 @@
+package apm.generics.linkedlist;
+
+public class Node <T> {
+	private T data;
+	public Node next;
+	
+	
+	public Node (T data)
+	{
+		this.data=data;
+		next=null;
+	}
+	
+	public T getData()
+	{
+		return data;
+	}
+
+}
